fe9a9d9fa2be0cd2239806909b3378cc7b3bca7b
Ddo7184 <dodavid279@gmail.com>
2025-11-19 16:33:11 -0600

    Update LoginBannerAuto.ps1

--- LoginBannerAuto.ps1
+++ LoginBannerAuto.ps1
@@ -11,8 +11,10 @@
 # Please test and verify the script can function correctly with PowerShell ISE(Admin) in a non-production environment.
 # If the script breaks, please inform me of what happened, preferrably with screenshots of errors.
 
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name legalnoticecaption -Value "AUTHORIZED USE ONLY"
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name legalnoticecaption -Value "WARNING: This system is for authorized use only."
 
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name legalnoticetext -Value "This system is for authorized use only. Unauthorized access is prohibited and may be subject to monitoring and prosecution. By using this system, you consent to such monitoring."
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name legalnoticetext -Value "By accessing this system, you acknowledge and consent that all activities are monitored, logged, and subject to audit.
+Unauthorized access or misuse is strictly prohibited and may result in disciplinary action, criminal prosecution, or both. If you are not an authorized user, disconnect immediately."
 
 gpupdate /force
+
